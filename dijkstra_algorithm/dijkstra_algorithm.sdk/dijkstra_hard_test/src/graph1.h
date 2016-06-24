28fe0f48aa5087f524412a39e5bed7bc86803740
buleks <buleks@gmail.com>
2016-06-24 11:21:07 +0200

    Finished working version

---
+++ dijkstra_algorithm/dijkstra_algorithm.sdk/dijkstra_hard_test/src/graph1.h
@@ -0,0 +1,15 @@
+/*
+ * graph1.h
+ *
+ *  Created on: 17 cze 2016
+ *      Author: buleks
+ */
+
+#ifndef GRAPH1_H_
+#define GRAPH1_H_
+
+extern uint8_t graph1[N][N];
+extern uint8_t graph1_res[N];
+void InitGraph1(void);
+
+#endif /* GRAPH1_H_ */
